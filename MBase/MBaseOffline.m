ec742e48a76014c56fd7ec3e641a0bad77a6d26b
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-08-01 08:33:19 -0500

    offline post caching

--- MBase/MBaseOffline.m
+++ MBase/MBaseOffline.m
@@ -14,11 +14,22 @@
 
 #import "MBaseOffline.h"
 #import "Reachability.h"
+#import "OfflineData.h"
+#import "SBJson.h"
 
 MBaseOffline * __MBASEOFFLINE_INSTANCE;
 
+@interface MBaseOffline()
+@property (strong) NSString *filename;
+@property (strong) NSMutableArray *queue;
+
+- (void) load;
+- (void) save;
+- (OfflineData *) dequeue;
+@end
+
 @implementation MBaseOffline
-@synthesize reachability;
+@synthesize reachability, filename, queue;
 
 + (MBaseOffline *) instance{
     @synchronized(self){
@@ -30,6 +41,15 @@ MBaseOffline * __MBASEOFFLINE_INSTANCE;
 
 + (void) setInstance:(MBaseOffline *)instance{
     __MBASEOFFLINE_INSTANCE = instance;
+}
+
+- (id) init{
+    self = [super init];
+    if(self){
+        self.filename = @"mbase_queue.json";
+        [self load];
+    }
+    return self;
 }
 
 - (void) setApiHost:(NSString *)hostname{
@@ -50,11 +70,68 @@ MBaseOffline * __MBASEOFFLINE_INSTANCE;
 }
 
 - (void) flushPendingUpdates{
-    //TODO
+    if(![reachability isReachable]){
+        return;
+    }
+    @synchronized(self){
+        OfflineData *item = [self dequeue];
+        [MBase postData:item.data toPath:item.path withAuthorization:item.authorization]; //if this fails, MBase will requeue
+    }
+    if([queue count] > 0){
+        [self performSelector:@selector(flushPendingUpdates) withObject:nil afterDelay:1];
+    }
 }
 
 - (void) cachePostData:(NSDictionary *)data toPath:(NSString *)path withAuthorization:authorization {
-    //TODO
+    @synchronized(self){
+        [queue addObject:@{@"data" : data, @"auth" : authorization, @"path" : path}];
+        [self save];
+    }
+}
+
+//--- "private"
+
+- (OfflineData *) dequeue{
+    OfflineData *result = nil;
+    @synchronized(self){
+        if([queue count] > 0){
+            NSDictionary *rawData = [queue objectAtIndex:0];
+            result = [OfflineData new];
+            result.data = [rawData objectForKey:@"data"];
+            result.path = [rawData objectForKey:@"path"];
+            result.authorization = [rawData objectForKey:@"auth"];
+            
+            [queue removeObjectAtIndex:0];
+            [self save];
+        }
+    }
+    return result;
+}
+
+- (void) load{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    NSString *libraryDirectory = [paths objectAtIndex:0];
+    NSString *file = [libraryDirectory stringByAppendingString:filename];
+    
+    NSData *rawData = [NSData dataWithContentsOfFile:file];
+    if(rawData){
+        NSString *json = [[NSString alloc] initWithData:rawData encoding:NSUTF8StringEncoding];
+        
+        SBJsonParser *parser = [[SBJsonParser alloc] init];
+        self.queue = [[NSMutableArray alloc] initWithArray:[parser objectWithString:json]];
+    }else{
+        self.queue = [NSMutableArray new];
+    }
+}
+
+- (void) save{
+    NSString *json = [queue JSONRepresentation];
+    NSData *data = [json dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    NSString *libraryDirectory = [paths objectAtIndex:0];
+    NSString *file = [libraryDirectory stringByAppendingString:filename];
+    [[NSFileManager defaultManager] createFileAtPath:file contents:data attributes:nil];
 }
 
 @end
---
+++ MBase/OfflineData.m
@@ -0,0 +1,13 @@
+//
+//  OfflineData.m
+//  MBase
+//
+//  Created by Jason Whitehorn on 8/1/13.
+//  Copyright (c) 2013 Waterfield Technologies. All rights reserved.
+//
+
+#import "OfflineData.h"
+
+@implementation OfflineData
+
+@end
